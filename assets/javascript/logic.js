5fe3e1f21c5769d73c0bf446a3f61d7ab8bfdf22
Dj Will Power <Dj Will Power>
2017-06-06 02:42:03 -0400

    adequate styling, needs logic to create the dynamic effects

---
+++ assets/javascript/logic.js
@@ -0,0 +1,22 @@
+var giphyUrl = "http://api.giphy.com/v1/gifs/";
+
+var searchTerm = "";
+
+var apiKey = "?api_key=dc6zaTOxFJmzC";
+
+var queryURL = giphyUrl + searchTerm + apiKey;
+
+ // 1. create the onclick function that queries the API with the button value as the searchTerm, and then display the gif results in the #gif-results <div> of the html.
+
+ $(".btn").on("click", function() {
+ 	searchTerm = $(this.value)
+
+ 	$.ajax({
+	url: queryURL,
+     method: 'GET'
+  }).done(function(response) {
+     console.log(response);
+  	});
+
+
+ })
