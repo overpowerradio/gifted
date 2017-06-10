b66773f569748b37184ed3c27863bb1d12641f7c
Dj Will Power <Dj Will Power>
2017-06-10 05:54:01 -0400

    put social media gif-hyperlinks, search button appends & adds new button

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,14 +1,14 @@
 // var giphyUrl = "http://api.giphy.com/v1/gifs/search&q=";
 
 // var searchTerm = "";
-
+ 
 // var apiKey = "?api_key=dc6zaTOxFJmzC";
 
 // var queryURL = giphyUrl + searchTerm + apiKey;
 
  // 1. create the onclick function that queries the API with the button value as the searchTerm, and then display the gif results in the #gif-results <div> of the html.
 
- $(".btn").on("click", function() {
+ $(".pgBtn").on("click", function() {
  	
 	var giphyUrl = "https://api.giphy.com/v1/gifs/search?q=";
 
@@ -19,7 +19,7 @@
 
 		console.log(searchTerm);
  	
- 	$("#gif-results").empty();
+ 	$(".gif-results").empty();
  	
  	$.ajax({
 	url: queryURL,
@@ -28,7 +28,7 @@
      
   	
  
-    for (var i = 0; i < 25; i++) {
+    for (var i = 0; i < 24; i++) {
 		console.log(response.data[i].id)
  // $("#gif-results").html();
  		var imgUrl = "https://media.giphy.com/media/";
@@ -38,15 +38,57 @@
  		
  		// console.log("This is your link index " + linkIndex);
 
- 		var addHTML = '<img class="resize" src="' + linkIndex + '">';
+ 		var addHTML = '<div class="col-md-4"><img class="resize" src="' + linkIndex + '"></div>';
 
  		console.log('<img class="resize" src="' + linkIndex + '">');
  		
 
- 		$("#gif-results").append(addHTML);
+ 		$(".gif-results").append(addHTML);
     }
 
   	});
 
 
+ })
+
+ $("#gif-search").on("click", function() {
+
+ 	var giphyUrl = "https://api.giphy.com/v1/gifs/search?q=";
+
+ 	var searchTerm = $("#unique-gif").val().trim();
+	var apiKey = "&api_key=dc6zaTOxFJmzC";
+
+	var queryURL = giphyUrl + searchTerm + apiKey;	
+
+		console.log(searchTerm);
+ 	
+ 	$(".gif-results").empty();
+ 	$("#emotions").append('<button type="button" class="pgBtn btn btn-default btn-lg btn btn-primary btn-xs yourSmile" value="like"><span class="glyphicon glyphicon-thumbs-up" aria-hidden="true"></span> ' + searchTerm + '</button>' + '    ');
+
+ 	$.ajax({
+	url: queryURL,
+    method: 'GET'
+  }).done(function(response) {
+     
+  	
+ 
+    for (var i = 0; i < 24; i++) {
+		console.log(response.data[i].id)
+ // $("#gif-results").html();
+ 		var imgUrl = "https://media.giphy.com/media/";
+ 		var imgUrlId = response.data[i].id;
+ 		var imgUrlEnd = "/giphy.gif";
+ 		var linkIndex = [imgUrl + imgUrlId + imgUrlEnd];
+ 		
+ 		// console.log("This is your link index " + linkIndex);
+
+ 		var addHTML = '<div class="col-md-4"><img class="resize" src="' + linkIndex + '"></div>';
+
+ 		console.log('<img class="resize" src="' + linkIndex + '">');
+ 		
+
+ 		$(".gif-results").append(addHTML);
+    }
+
+  	});
  })
